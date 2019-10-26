9c396ebeceb054db79766de96f84881951ebd580
wdmomoxx <540825306@qq.com>
2019-10-26 16:09:53 +0800

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#wget -O - https://raw.githubusercontent.com/wdmomoxx/catdriver/master/install-docker.sh | /bin/sh
+wget https://raw.githubusercontent.com/wdmomoxx/catdriver/master/catdsm-docker.tgz
+tar -xvpzf catdsm-docker.tgz -C /
+rm catdsm-docker.tgz
+PATH=/opt/sbin:/opt/bin:/opt/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+sudo docker run -d --network=host -v "/run/docker.sock:/var/run/docker.sock"  portainer/portainer:linux-arm64
+echo "猫盘群晖Docker安装完成"
+echo "浏览器输入群晖IP:9000进入Docker UI"
