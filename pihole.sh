c2898f49670eaf916fff8e61d4ce1b74d8dd0eb2
listenfree <chowchi@gmail.com>
2020-02-14 15:23:22 +0800

    Update pihole.sh

--- pihole.sh
+++ pihole.sh
@@ -1,3 +1,3 @@
 sudo ip link set eth0 promisc on
 sudo insmod /usr/lib/modules/macvlan.ko
-sudo docker run -d --net testnet -v /dev:/dev pihole/pihole
+sudo docker run -d --net macvlan -v /dev:/dev pihole/pihole
